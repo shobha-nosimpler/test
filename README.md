22584e455d007221ea2072b493410ed577aa81f6
shobha-nosimpler <shobha.mourya@gmail.com>
2018-05-14 11:16:12 +0530

    chips

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # test
 test repo for git commands demonstration
+git - clone status add commit log push 
+general - cd ls clear
--- hungry.py
+++ hungry.py
@@ -2,5 +2,6 @@ hungry = input("Are you hungry?")
 if hungry == "yes":
     print("Eat Samosa!")
     print(" And drink chilled lassi")
+    print(" And munch on Chips")
 else:
     print("Do your homework")
