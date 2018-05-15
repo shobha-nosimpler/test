d07fa9b407096579559e3b6126f78e4be0fb4cc5
shobha-nosimpler <shobha.mourya@gmail.com>
2018-05-15 11:40:04 +0530

    added yo

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry = input("Are you hungry?")
-if hungry == "yes":
+if hungry == "yes" or hungry == "yo":
     print("Eat Samosa!")
     print(" And drink chilled lassi")
     print(" And munch on Chips")
